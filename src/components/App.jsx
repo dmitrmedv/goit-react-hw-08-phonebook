5cd98db1351e342f2fb8f991ecf24f040121f258
dmitrmedv <dmitrmedv@gmail.com>
2023-11-10 23:08:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,6 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
     >
       React homework template
